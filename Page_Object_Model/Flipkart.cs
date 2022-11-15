6a09c9ab8276011782333bfdab0d9f8ae8a6b8b3
debadatta sarangi <debadattasarangi@gmail.com>
2022-11-15 13:22:03 +0530

    Add project files.

---
+++ Page_Object_Model/Flipkart.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exam.Page_Object_Model
+{
+    public class Flipkart
+    {
+        IWebDriver driver;
+        public Flipkart(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//input[contains(@title,'Search for products')]")]
+        public IWebElement Search;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='container']/div/div[1]/div[1]/div[2]/div[2]/form/div/button")]
+        public IWebElement Search_btn;
+
+        [FindsBy(How = How.ClassName, Using = "//*[@id='container']/div/div[3]/div[1]/div[2]/div[2]/div/div/div/a")]
+        public IWebElement Select;
+
+        [FindsBy(How = How.XPath, Using = "")]
+        public IWebElement AddToCart_btn;
+
+        [FindsBy(How = How.XPath, Using = "")]
+        public IWebElement Cart_btn;
+
+        public void Search_Ele()
+        {
+            Search.SendKeys("Mobile");
+            Search_btn.Click();
+            Select.Click();
+            
+
+
+        }
+    }
+}
---
+++ Support/Base.cs
@@ -0,0 +1,123 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Support
+{
+   public class Base
+    {
+       public IWebDriver driver;
+
+        public ExtentReports extent;
+        public ExtentTest test;
+        String browserName;
+        //report file
+        [OneTimeSetUp]
+        public void Setup()
+
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            String reportPath = projectDirectory + "//index.html";
+            var htmlReporter = new ExtentHtmlReporter(reportPath);
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+            extent.AddSystemInfo("Host Name", "Local host");
+            extent.AddSystemInfo("Environment", "QA");
+            extent.AddSystemInfo("Username", "Debadatta Sarangi");
+
+        }
+
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            test = extent.CreateTest(TestContext.CurrentContext.Test.Name);
+            string browserName = ConfigurationManager.AppSettings["browser"];
+            switch (browserName)
+            {
+                case ("Chrome"):
+                    driver = new ChromeDriver();
+                    driver.Navigate().GoToUrl("https://www.flipkart.com/");
+                    driver.Manage().Window.Maximize();
+                    Wait(); 
+                    break;
+                case ("Edge"):
+                    driver = new EdgeDriver();
+                    driver.Navigate().GoToUrl("https://www.flipkart.com/");
+                    driver.Manage().Window.Maximize();
+                    Wait();
+                    break;
+                default:
+                    driver = new ChromeDriver();
+                    driver.Navigate().GoToUrl("https://www.flipkart.com/");
+                    driver.Manage().Window.Maximize();
+                    Wait();
+                    break;
+
+
+            }
+        }
+
+        public void Wait()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stackTrace = TestContext.CurrentContext.Result.StackTrace;
+
+
+
+            DateTime time = DateTime.Now;
+            String fileName = "Screenshot_" + time.ToString("h_mm_ss") + ".png";
+
+            if (status == TestStatus.Failed)
+            {
+
+                test.Fail("Test failed", captureScreenShot(driver, fileName));
+                test.Log(Status.Fail, "test failed with logtrace" + stackTrace);
+
+            }
+            else if (status == TestStatus.Passed)
+            {
+
+            }
+
+            extent.Flush();
+            driver.Quit();
+
+        }
+
+        public MediaEntityModelProvider captureScreenShot(IWebDriver driver, String screenShotName)
+
+        {
+            ITakesScreenshot ts = (ITakesScreenshot)driver;
+            var screenshot = ts.GetScreenshot().AsBase64EncodedString;
+
+            return MediaEntityBuilder.CreateScreenCaptureFromBase64String(screenshot, screenShotName).Build();
+
+
+
+
+        }
+
+
+
+    }
+}
